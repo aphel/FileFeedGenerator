8ca876eb4493552b6823770f85f4b834f80a6528
Alex Maslov <mralexmaslov@gmail.com>
2015-12-19 23:42:30 +1300

    Run Update-Feed again "poc" dir in the build script

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,7 +1,12 @@
 function Start-TestShell {
     powershell.exe -NoExit -NoProfile `
     -Command {
+        # Import the module and verify it is imported
         Import-Module .\FileFeedGenerator
+        Get-Module FileFeedGenerator
+
+        Update-Feed -Path .\poc
+
         function prompt {
             Write-Host -NoNewline `
                 -ForegroundColor 'Red' `
