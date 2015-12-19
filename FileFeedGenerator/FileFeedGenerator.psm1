1a5f0fd2094c580f4aeebb2eee3d9ba9f58545f8
Alex Maslov <mralexmaslov@gmail.com>
2015-12-19 23:41:41 +1300

    Normalize Path parameter

--- FileFeedGenerator/FileFeedGenerator.psm1
+++ FileFeedGenerator/FileFeedGenerator.psm1
@@ -6,6 +6,9 @@ function Update-Feed {
 	[CmdletBinding()]
 	param(
 		[ValidateScript({
+			# Normalize path to absolute path
+			$_ = [IO.Path]::GetFullPath($_)
+
 			if (-not (Test-Path -PathType 'Container' $_)) {
 				throw "Path '${_}' does not exist or it in not a directory. Please provide a correct path or omit this parameter in order to use your current location."
 			}
