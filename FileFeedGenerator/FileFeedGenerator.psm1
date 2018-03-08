2df8cceb670dedc3b24fe5956d18e9e7c5b6c9a6
Alex Maslov <mralexmaslov@gmail.com>
2018-03-08 15:40:40 +1300

    WIP: filter out empty folders, rename sample rss

--- FileFeedGenerator/FileFeedGenerator.psm1
+++ FileFeedGenerator/FileFeedGenerator.psm1
@@ -1,20 +1,35 @@
 <#
-	My Function
+    My Function
 #>
 
 function Update-Feed {
-	[CmdletBinding()]
-	param(
-		[ValidateScript({
-			# Normalize path to absolute path
-			$_ = [IO.Path]::GetFullPath($_)
+  [CmdletBinding()]
+  param([ValidateScript({
+          # Normalize path to absolute path
+          $_ = [IO.Path]::GetFullPath($_)
 
-			if (-not (Test-Path -PathType 'Container' $_)) {
-				throw "Path '${_}' does not exist or it in not a directory. Please provide a correct path or omit this parameter in order to use your current location."
-			}
-			$true
-		})]
-		[string]$Path = (Get-Location)
-	)
-	Write-Output "Update feeds for directory: $Path"
+          if (-not (Test-Path -PathType 'Container' $_)) {
+            throw "Path '${_}' does not exist or it in not a directory. Please provide a correct path `
+            or omit this parameter in order to use your current location."
+          }
+          $true
+    })]
+
+    [string]$Path = (Get-Location)
+  )
+  
+  begin {
+    Write-Output "Update feeds for directory: $Path"
+  }
+  
+  process {
+    # get directories containing audio files
+    $mp3Directories = Get-ChildItem $Path -Directory |
+      Where-Object { (Get-ChildItem -Path $_.FullName -Filter '*.mp3').Length -gt 0 }
+    
+    #Write-Host 'Generate RSS stream for the following podcast: ' + $mp3Directories.PSChildName
+  }
+  
+  end {}
 }
+
